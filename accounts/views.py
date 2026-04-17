80652a92dd0cf423a3690e772653235a493e18e2
Samia Zaman <samiazaman174@gmail.com>
2026-04-17 12:23:45 +0600

    Added user authentication placeholder and forgot password functionality

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+﻿from django.shortcuts import render
+
+def login_page(request):
+    return render(request, 'auth_message.html', {'message': 'Successfully logged in!'})
+
+def signup_page(request):
+    return render(request, 'auth_message.html', {'message': 'Successfully signed up!'})
+
+def register_page(request):
+    return render(request, 'auth_message.html', {'message': 'Successfully registered!'})
